5c04dfb18749370ed7172a047b33a1c35bfec51e
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-07-19 12:06:11 +0200

    Get books from BooksAPI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
   state = {
+    books: []
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -13,6 +14,12 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
   };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({books});
+    });
+  }
 
   render() {
     return (
