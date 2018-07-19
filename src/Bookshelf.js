ded517362b12b30220b0359f2104ac38efda1638
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-07-19 11:48:05 +0200

    Pass first prop to Bookshelf component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
-
 class BookShelf extends Component {
   render() {
     return(
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{this.props.title}</h2>
+
+        </div>
+
+      </div>
 
     )
   }
 }
+export default BookShelf
