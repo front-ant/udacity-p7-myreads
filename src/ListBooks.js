8e70307142ecdf6540b50d45908ece73f3440aef
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-07-26 20:30:30 +0200

    Refactor into stateless functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,42 +1,36 @@
 import React, {Component} from 'react';
 import BookShelf from './Bookshelf';
 
-class ListBooks extends Component {
-  filterByShelf = shelf => {
-    const allBooks = this.props.books;
+const ListBooks = props => {
+  const filterByShelf = shelf => {
+    const allBooks = props.books;
     return allBooks.filter(book => book.shelf === shelf);
   };
 
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <BookShelf
-            title={'Currently Reading'}
-            shelvedBooks={this.filterByShelf('currentlyReading')}
-            onChangeShelf={this.props.onChangeShelf}
-          />
-          <BookShelf
-            title={'Want to Read'}
-            shelvedBooks={this.filterByShelf('wantToRead')}
-            onChangeShelf={this.props.onChangeShelf}
-          />
-          <BookShelf
-            title={'Read'}
-            shelvedBooks={this.filterByShelf('read')}
-            onChangeShelf={this.props.onChangeShelf}
-          />
-          <BookShelf
-            title={'Uncategorized'}
-            shelvedBooks={this.filterByShelf('none')}
-            onChangeShelf={this.props.onChangeShelf}
-          />
-        </div>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    );
-  }
-}
+      <div className="list-books-content">
+        <BookShelf
+          title={'Currently Reading'}
+          shelvedBooks={filterByShelf('currentlyReading')}
+          onChangeShelf={props.onChangeShelf}
+        />
+        <BookShelf
+          title={'Want to Read'}
+          shelvedBooks={filterByShelf('wantToRead')}
+          onChangeShelf={props.onChangeShelf}
+        />
+        <BookShelf
+          title={'Read'}
+          shelvedBooks={filterByShelf('read')}
+          onChangeShelf={props.onChangeShelf}
+        />
+      </div>
+    </div>
+  );
+};
+
 export default ListBooks;
