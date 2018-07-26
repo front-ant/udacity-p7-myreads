d110f84da026ef0f6f2458da391bf858f942d7f2
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-07-26 15:01:15 +0200

    Refactor into stateless functional components

--- src/Book.js
+++ src/Book.js
@@ -1,38 +1,36 @@
 import React, {Component} from 'react';
 import ShelfBook from './ShelfBook';
 
-class Book extends Component {
-  render() {
-    const {book} = this.props;
-    let bookImage;
-    if (book.imageLinks) {
-      bookImage = book.imageLinks.thumbnail;
-    } else {
-      bookImage =
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Book_question2.svg/200px-Book_question2.svg.png';
-    }
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url('${bookImage}')`
-            }}
-          />
-          <ShelfBook
-            onChangeShelf={this.props.onChangeShelf}
-            book={this.props.book}
-            shelf={this.props.shelf}
-          />
-        </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
-      </div>
-    );
+function Book(props) {
+  const {book} = props;
+  let bookImage;
+  if (book.imageLinks) {
+    bookImage = book.imageLinks.thumbnail;
+  } else {
+    bookImage =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Book_question2.svg/200px-Book_question2.svg.png';
   }
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url('${bookImage}')`
+          }}
+        />
+        <ShelfBook
+          onChangeShelf={props.onChangeShelf}
+          book={props.book}
+          shelf={props.shelf}
+        />
+      </div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors}</div>
+    </div>
+  );
 }
 
 export default Book;
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,28 +1,27 @@
 import React, {Component} from 'react';
 import Book from './Book';
 
-class BookShelf extends Component {
-  render() {
-    return (
-      <div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">{this.props.title}</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {this.props.shelvedBooks.map(book => (
-                <li key={book.id}>
-                  <Book
-                    book={book}
-                    onChangeShelf={this.props.onChangeShelf}
-                    shelf={book.shelf}
-                  />
-                </li>
-              ))}
-            </ol>
-          </div>
+function BookShelf(props) {
+  return (
+    <div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {props.shelvedBooks.map(book => (
+              <li key={book.id}>
+                <Book
+                  book={book}
+                  onChangeShelf={props.onChangeShelf}
+                  shelf={book.shelf}
+                />
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default BookShelf;
