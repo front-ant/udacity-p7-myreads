1b4316ba3b81978ae4ed3ca70be03d2d0404a285
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-07-28 11:02:17 +0200

    Remove unneeded component import

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,4 @@
-import React, {Component, PureComponent} from 'react';
+import React, {PureComponent} from 'react';
 import ShelfBook from './ShelfBook';
 
 class Book extends PureComponent {
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import BookShelf from './Bookshelf';
 
 const ListBooks = props => {
